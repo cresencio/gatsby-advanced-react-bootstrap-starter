a3cfb136e9667f8a7262bb7448d93f4a29da3de1
Cresencio Cantu <cresenciocantu@gmail.com>
2020-11-28 16:01:08 -0600

    Update Footer.jsx
    - Use bootstrap grid components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import UserLinks from "../UserLinks/UserLinks";
 import "./Footer.scss";
+import { Row, Col } from 'react-bootstrap';
 
 function Footer({ config }) {
   const url = config.siteRss;
@@ -10,23 +11,25 @@ function Footer({ config }) {
     return null;
   }
   return (
-    <footer className="footer">
+    <Row as="footer" className="footer bg-dark text-white">
+      <Col sm={6}>
       <UserLinks config={config} labeled />
-      <div className="notice-container">
-        <h4>{copyright}</h4>
+        <div className="notice-container">
+          <h4>{copyright}</h4>
 
-        <Link to={url}>
-          <button type="button">Subscribe</button>
-        </Link>
-        <h4>
-          Based on{" "}
-          <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
-            Gatsby Advanced Starter
-          </a>
-          .
-        </h4>
-      </div>
-    </footer>
+          <Link to={url}>
+            <button type="button">Subscribe</button>
+          </Link>
+          <h4>
+            Based on{" "}
+            <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
+              Gatsby Advanced Starter
+            </a>
+            .
+          </h4>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
