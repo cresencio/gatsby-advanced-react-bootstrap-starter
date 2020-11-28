85975cb5a4ef667b0e73da8580012dd9d9f277b7
Cresencio Cantu <cresenciocantu@gmail.com>
2020-11-28 16:00:50 -0600

    Update About page
    - Use bootstrap components and styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./About.scss";
+import { Col } from 'react-bootstrap';
 
 function About() {
   return (
-    <div className="about">
+    <Col className="about">
       <h1>
         Edit About component or pages/about.jsx to include your information.
       </h1>
-    </div>
+    </Col>
   );
 }
 
