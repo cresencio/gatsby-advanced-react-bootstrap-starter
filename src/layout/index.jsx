191a3bf4be09314861d3ea7ec26cbf0554f5bb07
Cresencio Cantu <cresenciocantu@gmail.com>
2020-11-28 16:02:13 -0600

    Update index.jsx
    - add footer to site layout component so that the footer shows on all pages/posts

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.scss";
+import {Container} from 'react-bootstrap';
+import Footer from "../components/Footer/Footer";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="layout-container">
+    <Container fluid>
       <Helmet>
         <meta name="description" content={config.siteDescription} />
         <html lang="en" />
       </Helmet>
       {children}
-    </div>
+      <Footer config={config} />
+    </Container>
   );
 }
